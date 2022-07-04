ff350b635fce059e41c34f3df5ee1119ae748f71
VincentGichohi <vincentegichohi@gmail.com>
2022-07-04 12:15:42 +0300

    Add a route handler

--- app.js
+++ app.js
@@ -19,6 +19,46 @@ const articleSchema = {
 //creating a new model and linking it to the schema
 const Article = mongoose.model("Article", articleSchema);
 
+//A route handler that eliminates redundancy in calling the methods
+app.route("/articles")
+    .get((req, res) => {
+        Article.find((err, foundArticles) => {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+            //Console logs the output to the terminal
+            // console.log(foundArticles);
+            //This now sends the result to the articles page in the browser
+        });
+    }).post((req, res) => {
+        console.log(req.body.title);
+        console.log(req.body.content);
+        //Create a new article which will be saved by posting it.
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        //when saving, check whether there is an error and post the error if there is one.
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("Successfully added a new article.")
+            } else {
+                res.send(err);
+            }
+        });
+    }).delete((req, res) => {
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Successfully deleted all articles")
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -28,45 +68,9 @@ app.use(express.static("public"));
 
 
 //A get method to retrieve data from the database
-app.get("/articles", (req, res) => {
-    Article.find((err, foundArticles) => {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-        //Console logs the output to the terminal
-        // console.log(foundArticles);
-        //This now sends the result to the articles page in the browser
-    });
-});
-app.delete("/articles", (req, res) => {
-    Article.deleteMany((err) => {
-        if (!err) {
-            res.send("Successfully deleted all articles")
-        } else {
-            res.send(err);
-        }
-    });
-});
-app.post("/articles", (req, res) => {
-    console.log(req.body.title);
-    console.log(req.body.content);
-    //Create a new article which will be saved by posting it.
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-
-    //when saving, check whether there is an error and post the error if there is one.
-    newArticle.save((err) => {
-        if (!err) {
-            res.send("Successfully added a new article.")
-        } else {
-            res.send(err);
-        }
-    });
-});
+app.get("/articles");
+app.delete("/articles");
+app.post("/articles");
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
